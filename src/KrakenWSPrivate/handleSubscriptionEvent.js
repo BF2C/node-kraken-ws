ee4bf6cc19ebf8bd8c2c9a586a56884fb4c9f543
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-11-20 13:44:37 +0100

    fix(subscription event): forward payload directly

--- src/KrakenWSPrivate/handleSubscriptionEvent.js
+++ src/KrakenWSPrivate/handleSubscriptionEvent.js
@@ -1,11 +1,4 @@
 export const handleSubscriptionEvent = ({ payload }) => {
   if (!Array.isArray(payload)) return
-
-  const event = {
-    data: payload[0],
-    name: payload[1],
-    meta: payload[2],
-  }
-
-  return { name: 'kraken:subscription:event', payload: event }
+  return { name: 'kraken:subscription:event', payload }
 }
--- src/KrakenWSPublic/handleSubscriptionEvent.js
+++ src/KrakenWSPublic/handleSubscriptionEvent.js
@@ -1,12 +1,4 @@
 export const handleSubscriptionEvent = ({ payload }) => {
   if (!Array.isArray(payload) || !Number.isInteger(payload[0])) return
-
-  const event = {
-    channelID: payload[0],
-    data: payload[1],
-    name: payload[2],
-    pair: payload[3],
-  }
-
-  return { name: 'kraken:subscription:event', payload: event }
+  return { name: 'kraken:subscription:event', payload }
 }
